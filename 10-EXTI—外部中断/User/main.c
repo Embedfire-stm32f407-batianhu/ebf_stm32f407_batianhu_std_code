9730c8cb305ce92784891cbad8af609a8ad1f6d3
flyleaf91 <flyleaf91@163.com>
2017-01-04 18:01:58 +0800

    添加外部中断例程

---
+++ 10-EXTI—外部中断/User/main.c
@@ -0,0 +1,47 @@
+/**
+  ******************************************************************************
+  * @file    main.c
+  * @author  fire
+  * @version V1.0
+  * @date    2015-xx-xx
+  * @brief   使用外部中断EXTI检测按键，控制彩灯。
+  ******************************************************************************
+  * @attention
+  *
+  * 实验平台:秉火  STM32 F407 开发板
+  * 论坛    :http://www.firebbs.cn
+  * 淘宝    :https://fire-stm32.taobao.com
+  *
+  ******************************************************************************
+  */
+  
+#include "stm32f4xx.h"
+#include "./led/bsp_led.h"
+#include "./key/bsp_exti.h"
+
+void Delay(__IO u32 nCount); 
+
+/**
+  * @brief  主函数
+  * @param  无
+  * @retval 无
+  */
+int main(void)
+{
+	/* LED 端口初始化 */
+	LED_GPIO_Config();
+  	
+	/* 初始化EXTI中断，按下按键会触发中断，
+  *  触发中断会进入stm32f4xx_it.c文件中的函数
+	*  KEY1_IRQHandler和KEY2_IRQHandler，处理中断，反转LED灯。
+	*/
+	EXTI_Key_Config(); 
+	
+	/* 等待中断，由于使用中断方式，CPU不用轮询按键 */
+	while(1)                            
+	{
+	}
+}
+
+/*********************************************END OF FILE**********************/
+
