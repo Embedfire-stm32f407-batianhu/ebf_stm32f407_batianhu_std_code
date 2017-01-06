fe8261148bf8d97d723f87d456255e0cb771e538
flyleaf91 <flyleaf91@163.com>
2017-01-06 17:00:52 +0800

    修改429为407

--- RS485通信实验/程序/User/main.c
+++ RS485通信实验/程序/User/main.c
@@ -8,7 +8,7 @@
   ******************************************************************************
   * @attention
   *
-  * 实验平台:秉火  STM32 F429 开发板 
+  * 实验平台:秉火  STM32 F407 开发板 
   * 论坛    :http://www.firebbs.cn
   * 淘宝    :https://fire-stm32.taobao.com
   *
@@ -46,7 +46,7 @@ int main(void)
 	
 	LED_BLUE;	
 	
-	printf("\r\n 欢迎使用秉火  STM32 F429 开发板。\r\n");
+	printf("\r\n 欢迎使用秉火  STM32 F407 开发板。\r\n");
   printf("\r\n 秉火F429 485通讯实验例程\r\n");
 	
 	printf("\r\n 实验步骤：\r\n");
