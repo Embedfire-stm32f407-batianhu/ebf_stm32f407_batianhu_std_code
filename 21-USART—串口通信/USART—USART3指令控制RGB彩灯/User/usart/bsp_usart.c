2baa8c96599de29739d15cff238d85010b3a41bc
nebulamagic <892461474@qq.com>
2021-01-27 16:50:38 +0800

    补充遗留U3

---
+++ 21-USART—串口通信/USART—USART3指令控制RGB彩灯/User/usart/bsp_usart.c
@@ -0,0 +1,98 @@
+/**
+  ******************************************************************************
+  * @file    bsp_usart.c
+  * @author  fire
+  * @version V1.0
+  * @date    2015-xx-xx
+  * @brief   重定向c库printf函数到usart端口
+  ******************************************************************************
+  * @attention
+  *
+  * 实验平台:野火  STM32 F407 开发板  
+  * 论坛    :http://www.firebbs.cn
+  * 淘宝    :https://fire-stm32.taobao.com
+  *
+  ******************************************************************************
+  */ 
+  
+#include "./usart/bsp_usart.h"
+
+
+ /**
+  * @brief  USART GPIO 配置,工作模式配置。115200 8-N-1 
+  * @param  无
+  * @retval 无
+  */
+void USARTx_Config(void)
+{
+  GPIO_InitTypeDef GPIO_InitStructure;
+  USART_InitTypeDef USART_InitStructure;
+		
+  RCC_AHB1PeriphClockCmd(USARTx_RX_GPIO_CLK|USARTx_TX_GPIO_CLK,ENABLE);
+
+  /* 使能 USART 时钟 */
+  USARTx_CLOCKCMD(USARTx_CLK, ENABLE);
+  
+  /* GPIO初始化 */
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;  
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  
+  /* 配置Tx引脚为复用功能  */
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+  GPIO_InitStructure.GPIO_Pin =  USARTx_TX_PIN  ;  
+  GPIO_Init(USARTx_TX_GPIO_PORT, &GPIO_InitStructure);
+
+  /* 配置Rx引脚为复用功能 */
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+  GPIO_InitStructure.GPIO_Pin =  USARTx_RX_PIN;
+  GPIO_Init(USARTx_RX_GPIO_PORT, &GPIO_InitStructure);
+  
+ /* 连接 PXx 到 USARTx_Tx*/
+  GPIO_PinAFConfig(USARTx_RX_GPIO_PORT,USARTx_RX_SOURCE,USARTx_RX_AF);
+
+  /*  连接 PXx 到 USARTx__Rx*/
+  GPIO_PinAFConfig(USARTx_TX_GPIO_PORT,USARTx_TX_SOURCE,USARTx_TX_AF);
+  
+  /* 配置串DEBUG_USART 模式 */
+  /* 波特率设置：DEBUG_USART_BAUDRATE */
+  USART_InitStructure.USART_BaudRate = USARTx_BAUDRATE;
+  /* 字长(数据位+校验位)：8 */
+  USART_InitStructure.USART_WordLength = USART_WordLength_8b;	
+  /* 停止位：1个停止位 */
+  USART_InitStructure.USART_StopBits = USART_StopBits_1;
+  /* 校验位选择：偶校验 */  
+	USART_InitStructure.USART_Parity = USART_Parity_No;
+  /* 硬件流控制：不使用硬件流 */
+  USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+  /* USART模式控制：同时使能接收和发送 */
+  USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+  /* 完成USART初始化配置 */
+  USART_Init(USARTx, &USART_InitStructure); 
+	
+  /* 使能串口 */
+  USART_Cmd(USARTx, ENABLE);
+}
+
+///重定向c库函数printf到串口，重定向后可使用printf函数
+int fputc(int ch, FILE *f)
+{
+		/* 发送一个字节数据到串口 */
+		USART_SendData(USARTx, (uint8_t) ch);
+		
+		/* 等待发送完毕 */
+		while (USART_GetFlagStatus(USARTx, USART_FLAG_TXE) == RESET);		
+	
+		return (ch);
+}
+
+///重定向c库函数scanf到串口，重写向后可使用scanf、getchar等函数
+int fgetc(FILE *f)
+{
+		/* 等待串口输入数据 */
+		while (USART_GetFlagStatus(USARTx, USART_FLAG_RXNE) == RESET);
+
+		return (int)USART_ReceiveData(USARTx);
+}
+
+/*********************************************END OF FILE**********************/
